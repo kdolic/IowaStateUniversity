ea114cf9cbe4e18ad0c5f7e49153261871132b47
Kenan Dolic <36944457+kdolic@users.noreply.github.com>
2020-06-08 00:03:41 -0500

    Create encrypt2.py

---
+++ MIS 407 - Python Programming/encrypt2.py
@@ -0,0 +1,13 @@
+kdolic@gbb1122-70 MINGW64 /t/MIS407/mis407s20-student-06/IA01-Ex1 (master)
+$ python encrypt.py
+Enter key (26 letters): nopqrstuvwxyzabcdefghijklm
+Enter text to encrypt: this is a test for my MIS class for my last semester
+Encrypted text:  guvf vf n grfg sbe zl zvf pynff sbe zl ynfg frzrfgre
+
+kdolic@gbb1122-70 MINGW64 /t/MIS407/mis407s20-student-06/IA01-Ex1 (master)
+$ python encrypt.py
+Enter key (26 letters): nopqrstuvwxyzabcdefghijklm
+Enter text to encrypt: guvf vf n grfg sbe zl zvf pynff sbe zl ynfg frzrfgre
+Encrypted text:  this is a test for my mis class for my last semester
+
+kdolic@gbb1122-70 MINGW64 /t/MIS407/mis407s20-student-06/IA01-Ex1 (master)
