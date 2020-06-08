a472e0698c79af9a2cfbaa639d76790fd47f8574
Kenan Dolic <36944457+kdolic@users.noreply.github.com>
2020-06-07 23:41:55 -0500

    Create Homework1.java

---
+++ COM S 207 - Fundamentals of Computer Programming/Homework1.java
@@ -0,0 +1,27 @@
+package homework.pkg1;
+
+/**
+ *
+ * @author kdolic
+ * 1.) Write a program that prints a greeting of your choice. 
+ * The greeting can be in any language of your choice.
+ * 
+ * 2. Write a program that prints the sum of 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1
+ * 
+ */
+public class Homework1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+     //#1
+      String greeting = "Hello, my name is Kenan Dolic.";
+      System.out.println(greeting);
+     //#2   
+        int sum = 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1;
+       System.out.println(sum);
+    }
+    
+}
